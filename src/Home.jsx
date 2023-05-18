c4bce88248ff246e74a9a3cb596799fdf88e7b92
Portal <juanportalg@gmail.com>
2023-05-18 16:31:43 -0700

    Home Component created

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HomeHeader } from './HomeHeader'
+import { HomeSection } from './HomeSection'
+
+
+export const Home = () => {
+  return (
+    <>
+        <HomeHeader />
+        <HomeSection />
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Home } from './Home'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
