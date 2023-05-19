ede91f11ec007169327bf223390da4ef6f8dcbf2
Portal <juanportalg@gmail.com>
2023-05-18 19:30:51 -0700

    phone number added

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,9 +6,9 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons'
 export const Contact = () => {
   return (
     <section className='contact'>
+        <a href="tel:+13233568725"><FontAwesomeIcon icon={faPhone} /></a>
         <a href="https://www.instagram.com/jaytheehandyman/" target='_blank'><FontAwesomeIcon icon={faInstagram} /></a>
         <a href="https://wa.me/+13233568725?text=I%20want%20to%20remodel%20my%20kitchen" target='_blank'><FontAwesomeIcon icon={faWhatsapp} /></a>
-        <a href="tel:+17472947712"><FontAwesomeIcon icon={faPhone} /></a>
     </section>
   )
 }
