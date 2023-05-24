2807c9fe47fff7f50731a5a68f6a1048777927f1
Portal <juanportalg@gmail.com>
2023-05-24 00:02:12 -0700

    Home updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,14 @@ export const Home = () => {
   return (
     <section className='home'>
       <div className="section-container">
-        Home
+        <div>
+          <img src="./assets/commercial.jpg" alt="" />
+          <p>Commercial</p>
+        </div>
+        <div>
+          <img src="./assets/residential.jpg" alt="" />
+          <p>Residential</p>
+        </div>
       </div>
     </section>
   )
